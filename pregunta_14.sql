787dfc7fcc66f852f45dc527196e0bec1b27e81e
alejoesc96 <126002637+alejoesc96@users.noreply.github.com>
2023-04-15 21:38:11 -0500

    Update pregunta_14.sql

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -43,4 +43,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
-
+select tbl1.K0, avg(tbl2.c21) as 'avg(c21)' from tbl1, tbl2 on tbl1.K1 = tbl2.K1 where tbl1.c13 > 400 group by tbl1.K0;
